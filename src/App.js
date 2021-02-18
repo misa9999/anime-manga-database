fa4452dee18cd3a647048a1a51f22d5e828aafb4
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-18 19:37:05 -0300

    add: added Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState('');
+
   return (
     <div>
       <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <h3>Top anime</h3>
+        <a href="#" target="_blank" rel="noreferrer">Gintama</a>
+        <a href="#" target="_blank" rel="noreferrer">One Piece</a>
+        <a href="#" target="_blank" rel="noreferrer">Jujutsu Kaisen</a>
+      </nav>
+    </aside>
+  )
+}
+
+export default Sidebar;

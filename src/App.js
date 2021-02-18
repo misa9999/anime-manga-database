6f342c452627b09c64803c2843d1ed78c9513b9b
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-18 18:45:18 -0300

    add: added Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1><strong>Anime</strong>Search</h1>
+    </header>
+  )
+}
+
+export default Header;

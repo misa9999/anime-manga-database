abb1c273ab2fa97e6cb59fbedcd7b712065546a3
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-19 16:06:42 -0300

    add: added image to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import Img01 from '../assets/images/img-01.png';
+
 function Header({ value, name }) {
   console.log(value)
   return (
     <header className={value} >
+      {value && <img src={Img01} alt="" className="img-home" />}
+
       <h1><strong>{name}</strong>Search</h1>
     </header>
   )

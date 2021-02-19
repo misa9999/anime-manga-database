f49c9db17ce2b8d510bc915f183b54bde8b46eef
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-19 18:54:09 -0300

    feat: created loading screen

--- src/pages/AnimeSearch/AnimeSearch.js
+++ src/pages/AnimeSearch/AnimeSearch.js
@@ -6,10 +6,14 @@ import MainContent from '../../components/MainContent'
 
 import '../../assets/scss/main.css';
 
+import Lottie from "react-lottie";
+import animationData from "../../lottie/menhera-chan-2.json";
+
 function AnimeSearch() {
   const [animeList, setAnimeList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const getTopAnime = async () => {
     const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
@@ -35,6 +39,13 @@ function AnimeSearch() {
     getTopAnime();
   }, [])
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5500);
+  }, []);
+
   let progress = document.getElementById('progressbar');
   let totalHeight = document.body.scrollHeight - window.innerHeight;
   window.onscroll = function(){
@@ -42,8 +53,29 @@ function AnimeSearch() {
     progress.style.height = progressHeight + '%';
   }
 
+  const LoadingAnimation = () => {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+
+    return (
+      <div className="loading">
+        <Lottie options={defaultOptions} height={250} width={250} />
+      </div>
+    );
+  };
+
   return (
-    <div>
+    <>
+    {loading ? (
+      LoadingAnimation()
+    ) : (
+      <div>
       <div id="progressbar"></div>
       <div id="scrollPath"></div>
       <Header name={'Anime'} />
@@ -57,6 +89,8 @@ function AnimeSearch() {
         />
       </div>
     </div>
+    )}
+    </>
   )
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,57 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Header from "../../components/Header";
 import "../../assets/scss/main.css";
 
+import Lottie from "react-lottie";
+import animationData from "../../lottie/menhera-chan-1.json";
+
 function Home() {
-  return (
-    <div className="home-container">
-      <Header value={'header-home'} name={'Anime&Manga'} />
-      <div className="button-container">
-        <Link to="/anime-search" className="button-anime">
-          ANIME
-        </Link>
-        <Link to="/manga-search" className="button-manga">
-          MANGA
-        </Link>
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5500);
+  }, []);
+
+  const LoadingAnimation = () => {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+
+    return (
+      <div className="loading">
+        <Lottie options={defaultOptions} height={250} width={250} />
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <>
+      {loading ? (
+        LoadingAnimation()
+      ) : (
+        <div className="home-container">
+          <Header value={"header-home"} name={"Anime&Manga"} />
+          <div className="button-container">
+            <Link to="/anime-search" className="button-anime">
+              ANIME
+            </Link>
+            <Link to="/manga-search" className="button-manga">
+              MANGA
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 

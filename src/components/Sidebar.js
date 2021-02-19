f86dbb46f9f66f3b392cbda901a61be3941c138e
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-19 11:58:01 -0300

    fix: fixed aside position

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,33 +2,41 @@ function Sidebar({ topAnime, topManga }) {
   return (
     <aside>
       <nav>
-        <h3>Top anime</h3>
-        {topAnime.map(anime => (
-          <a 
-            href={anime.url} 
-            target="_blank" 
-            key={anime.mal_id}
-            rel="noreferrer"
-          >
-            {anime.title}
-          </a>
-        ))}
+        <div className="top-anime-container">
+          <div>
+            <h3>Top anime</h3>
+            {topAnime.map((anime) => (
+              <a
+                href={anime.url}
+                target="_blank"
+                key={anime.mal_id}
+                rel="noreferrer"
+              >
+                {anime.title}
+              </a>
+            ))}
+          </div>
+        </div>
       </nav>
       <nav>
-        <h3>Top manga</h3>
-        {topManga.map(manga => (
-          <a
-            href={manga.url}
-            target="_blank"
-            key={manga.mal_id}
-            rel="noreferrer"
-          >
-            {manga.title}
-          </a>
-        ))}
+        <div className="top-manga-container">
+          <div>
+            <h3>Top manga</h3>
+            {topManga.map((manga) => (
+              <a
+                href={manga.url}
+                target="_blank"
+                key={manga.mal_id}
+                rel="noreferrer"
+              >
+                {manga.title}
+              </a>
+            ))}
+          </div>
+        </div>
       </nav>
     </aside>
-  )
+  );
 }
 
 export default Sidebar;

8224ab1b0a41bd4e72d4f532f3e983a623bde803
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-19 21:41:21 -0300

    fix: changed title on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header({ value, name }) {
   console.log(value)
   return (
     <header className={value} >
-      <h1 className={value ? "title-home" : ''}><strong>{name}</strong>Search</h1>
+      <h1 className={value ? "title-home" : ''}><strong>{name}</strong>Database</h1>
       {value && <img src={Img02} alt="" className="img-home" />}
     </header>
   )
